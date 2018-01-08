7aeea45839c609f19b3c216a6e6e0f64b82d87e8
KongXiangX <760736325@qq.com>
2018-01-08 13:32:31 +0800

    Initial Commit

---
+++ KXAccountRecord/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KXAccountRecord
+//
+//  Created by apple on 2018/1/8.
+//  Copyright © 2018年 KX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KXAccountRecord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KXAccountRecord
+//
+//  Created by apple on 2018/1/8.
+//  Copyright © 2018年 KX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
